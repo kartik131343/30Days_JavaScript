1c99024fb26de6e53af3965cd0f064acdc7a9450
kartik kumar <111492499+kartik131343@users.noreply.github.com>
2024-07-15 23:28:42 +0530

    Add files via upload

---
+++ Day2/Operators.js
@@ -0,0 +1,96 @@
+// Day 2: Operators
+
+
+// Activity 1: Arithmetic Operations
+
+
+// • Task 1: Write a program to add two numbers and log the result to the console.
+var a,b;
+a=20; b=50; 
+c=a+b;
+console.log(c);
+
+// • Task 2: Write a program to subtract two numbers and log the result to the console.
+d=a-b;
+console.log(d);
+
+// • Task 3: Write a program to multiply two numbers and log the result to the console.
+e=a*b;
+console.log(e);
+
+// • Task 4: Write a program to divide two numbers and log the result to the console.
+f=a/b;
+console.log(f);
+
+// • Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+g=a%b;
+console.log(g);
+
+
+
+
+// Activity 2: Assignment Operators
+
+
+// • Task 6: Use the += operator to add a number to a variable and log the result to the console.
+a+=a+b;
+console.log(a);
+
+// • Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+a-=a+b;
+console.log(a);
+
+
+
+// Activity 3: Comparison Operators
+
+
+// • Task 8: Write a program to compare two numbers using › and ‹ and log the result to the console.
+max=100; min=30;
+if(max > min ? max+=max+min : max-=max-min);
+console.log(max);
+
+// • Task 9: Write a program to compare two numbers using ›= and ‹= and log the result to the console.
+if(max >= min){
+    console.log(max);
+}else{
+    console.log(min);
+}
+
+// • Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+Age = 12; Age1 = "12";
+console.log(Age==Age1); //Output - true
+console.log(Age===Age1); //Output - false
+
+
+// Activity 4: Logical Operators
+
+
+// • Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+var Name = 'kartik'; Age = 12;
+if(Name == 'kartik' && Age == 12){
+    console.log(true);
+}else{                                  //Output - true
+    console.log(false);
+}
+
+// • Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+if(Name == 'kartik' || Age == 32){
+    console.log(true);
+}else{                                  //Output - true
+    console.log(false);
+}
+
+// • Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+if(Name == 'kartik' && Age != 12){
+    console.log(true);
+}else{                                  //Output - false
+    console.log(false);
+}
+
+// Activity 5: Ternary Operator
+
+
+// • Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let number = -10;
+console.log(number > 0 ? "Positive" : "Negative");  //Output - Negative
